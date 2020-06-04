32ad4953fd842665935a95812fb8e58d46ff2808
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-04 13:20:16 -0700

    add readme file, add functionality to scripts.js file, add styles to styles.css file
    co-authored-by: NoelKirkland <noelkirkland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,30 @@ $(document).ready(function(){
     const age = parseInt($("input#age").val());
     const pref = $("input:radio[name=pref]:checked").val();
     const astrological = $("#astro").val();
-    if(age){
+    if(!age){
       alert("Please input your age in numbers");
     }
     else if(age < 0 || age > 150){
       alert("Incorrect age input (you are not born yet or you are already dead)");
     }
     else{
-
+      if(pref === "star-wars" && (astrological === "Aries" || astrological === "Taurus" || astrological === "Gemini" ||
+      astrological === "Cancer" || astrological === "Leo" || astrological === "Virgo")){
+        $("#match").text("Darth Vador");
+      }
+      else if(pref === "marvel" && (astrological === "Aries" || astrological === "Taurus" || astrological === "Gemini" ||
+      astrological === "Cancer" || astrological === "Leo" || astrological === "Virgo")){
+        $("#match").text("Spider Man");
+      }
+      else if(pref === "star-wars" && (astrological === "Libra" || astrological === "Scorpio" || astrological === "Sagittarius" ||
+      astrological === "Capricorn" || astrological === "Aquarius" || astrological === "Pisces")){
+        $("#match").text("Yoda");
+      }
+      else{
+        $("#match").text("Thanos");
+      }
+      $("#answer").show();
     }
-
     event.preventDefault();
   });
-
 });
