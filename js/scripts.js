216b059566927b85d16285b077b4367085630e38
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-04 12:04:03 -0700

    add three form sections to the html file, hide the answer div in styles.css, add functionality to scripts.js
    co-authored-by: NoelKirkland <noelkirkland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+  $("form#info").submit(function(event){
+    const age = parseInt($("input#age").val());
+    const pref = $("input:radio[name=pref]:checked").val();
+    const astrological = $("#astro").val();
+    if(age){
+      alert("Please input your age in numbers");
+    }
+    else if(age < 0 || age > 150){
+      alert("Incorrect age input (you are not born yet or you are already dead)");
+    }
+    else{
+
+    }
+
+    event.preventDefault();
+  });
 
 });
